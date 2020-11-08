6082910ce052458624664b632b4c0bda8b2f4a0f
Rupam Sau <rupamsau0010@users.noreply.github.com>
2020-11-08 11:59:41 +0530

    Complete backend for Auth with register and login

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema(
   {
@@ -25,11 +25,20 @@ const userSchema = mongoose.Schema(
   {
     timestamps: true,
   }
-)
+);
 
-userSchema.methods.matchPassword = async function(enterPassword){
-  return await bcrypt.compare(enterPassword, this.password)
-}
+userSchema.methods.matchPassword = async function (enterPassword) {
+  return await bcrypt.compare(enterPassword, this.password);
+};
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userSchema);
 
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { getUser } = require("../controllers/userControllers")
+const { getUser, getUserProfile, registerUser } = require("../controllers/userControllers");
+const protect = require("../middlewares/authMiddleware");
 
-router.post("/login", getUser);
+router.post("/login", getUser)
+router.post("/", registerUser)
+router.get("/profile", protect, getUserProfile)
 
 module.exports = router;
