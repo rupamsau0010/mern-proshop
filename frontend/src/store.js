1829521fbeb68d1501aae8dd7525e9ec63a61492
Rupam Sau <rupamsau0010@users.noreply.github.com>
2020-11-13 21:07:44 +0530

    User Login Reducers and Actions

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,23 +1,39 @@
-import { createStore, combineReducers, applyMiddleware} from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-import { productListReducers, productDetailsReducers } from "./reducers/productReducers";
+import {
+  productListReducers,
+  productDetailsReducers,
+} from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
+import { userLoginReducers } from "./reducers/userReduces";
 
 const reducer = combineReducers({
-    productList: productListReducers,
-    productDetails: productDetailsReducers,
-    cart: cartReducer
+  productList: productListReducers,
+  productDetails: productDetailsReducers,
+  cart: cartReducer,
+  userLogin: userLoginReducers,
 });
 
-const cartItemsFromStroge = localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : []
+const cartItemsFromStroge = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+
+const userInfoFromStroge = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
 
 const initialState = {
-    cart: { cartItems: cartItemsFromStroge }
+  cart: { cartItems: cartItemsFromStroge },
+  userLogin: {userInfo: userInfoFromStroge}
 };
 
 const middleware = [thunk];
 
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 export default store;
