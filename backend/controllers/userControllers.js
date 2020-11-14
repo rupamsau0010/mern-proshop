223c7739df917f4fdf693233c4af2e8601114ec3
Rupam Sau <rupamsau0010@users.noreply.github.com>
2020-11-14 23:34:02 +0530

    Update Profile Route on the Backend

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -68,5 +68,32 @@ const getUserProfile = asyncHandler(async(req, res) => {
     }
 });
 
+// Update User Profile
 
-module.exports = {getUser, registerUser, getUserProfile}
+const updateUserProfile = asyncHandler(async(req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if(user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+        if(req.body.password) {
+            user.password = req.body.password
+        }
+
+        const updatedUser = await user.save()
+
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+            token: generateToken(user._id)
+        });
+    } else {
+        res.status(404)
+        throw new Error("User not Found")
+    }
+});
+
+
+module.exports = {getUser, registerUser, getUserProfile, updateUserProfile}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getUser, getUserProfile, registerUser } = require("../controllers/userControllers");
+const { getUser, getUserProfile, registerUser, updateUserProfile } = require("../controllers/userControllers");
 const protect = require("../middlewares/authMiddleware");
 
 router.post("/login", getUser)
 router.post("/", registerUser)
 router.get("/profile", protect, getUserProfile)
+router.put("/profile", protect, updateUserProfile)
 
 module.exports = router;
